186a53b2bef1d542d5490022125f428dc9426e6e
smail8 <smail8@github.com>
2020-08-02 18:05:48 +0200

    final commit

---
+++ Multi-Task-PV-LSTM.py
@@ -0,0 +1,263 @@
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import recall_score, accuracy_score, average_precision_score, precision_score
+
+import DataLoader
+import networks
+import utils
+
+
+class args():
+    def __init__(self):
+        self.jaad_dataset = '../../../../data/haziq-data/jaad/annotations'
+        self.dtype        = 'train'
+        self.from_file    = True
+        self.file         = '/data/smail-data/jaad_train_16_16_n.csv'
+        self.val_file     = '/data/smail-data/jaad_val_16_16_n.csv'
+        self.loader_workers = 10
+        self.loader_shuffle = True
+        self.pin_memory     = False
+        self.image_resize   = [240, 426]
+        self.device         = 'cuda'
+        self.batch_size     = 100
+        self.n_epochs       = 100
+        self.hidden_size    = 512
+        self.hardtanh_limit = 100
+        self.sample         = False
+        self.n_train_sequences = 40000
+        self.trainOrVal = 'train'
+        self.citywalks  = False
+        self.input  = 16
+        self.output = 16
+        self.skip   = 1
+        self.task   = 'bounding_box-intention'
+        self.use_scenes = False
+        self.lr = 0.00001
+
+args = args()
+
+
+net = networks.PV_LSTM(args).to(args.device)
+train, val, test = DataLoader.data_loader(args)
+
+
+optimizer = optim.Adam(net.parameters(), lr=args.lr)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=15,
+                                                      threshold = 1e-8, verbose=True)
+mse = nn.MSELoss()
+bce = nn.BCELoss()
+train_s_scores = []
+train_c_scores = []
+val_s_scores   = []
+val_c_scores   = []
+
+print('='*100)
+print('Training ...')
+for epoch in range(args.n_epochs):
+    start = time.time()
+
+    avg_epoch_train_s_loss = 0
+    avg_epoch_val_s_loss   = 0
+    avg_epoch_train_c_loss = 0
+    avg_epoch_val_c_loss   = 0
+
+    ade  = 0
+    fde  = 0
+    aiou = 0
+    fiou = 0
+    avg_acc = 0
+    avg_rec = 0
+    avg_pre = 0
+    mAP = 0
+
+    counter = 0
+    for idx, (obs_s, target_s, obs_p, target_p, target_c, label_c) in enumerate(train):
+        counter += 1
+        obs_s    = obs_s.to(device='cuda')
+        target_s = target_s.to(device='cuda')
+        obs_p    = obs_p.to(device='cuda')
+        target_p = target_p.to(device='cuda')
+        target_c = target_c.to(device='cuda')
+
+        net.zero_grad()
+        speed_preds, crossing_preds = net(speed=obs_s, pos=obs_p)
+        speed_loss  = mse(speed_preds, target_s)/100
+
+        crossing_loss = 0
+        for i in range(target_c.shape[1]):
+            crossing_loss += bce(crossing_preds[:,i], target_c[:,i])
+
+        crossing_loss /= target_c.shape[1]
+
+        loss = speed_loss + crossing_loss
+        loss.backward()
+        optimizer.step()
+
+        avg_epoch_train_s_loss += float(speed_loss)
+        avg_epoch_train_c_loss += float(crossing_loss)
+
+    avg_epoch_train_s_loss /= counter
+    avg_epoch_train_c_loss /= counter
+    train_s_scores.append(avg_epoch_train_s_loss)
+    train_c_scores.append(avg_epoch_train_c_loss)
+
+    counter=0
+    state_preds = []
+    state_targets = []
+    intent_preds = []
+    intent_targets = []
+    for idx, (obs_s, target_s, obs_p, target_p, target_c, label_c) in enumerate(test):
+        counter+=1
+        obs_s    = obs_s.to(device='cuda')
+        target_s = target_s.to(device='cuda')
+        obs_p    = obs_p.to(device='cuda')
+        target_p = target_p.to(device='cuda')
+        target_c = target_c.to(device='cuda')
+
+        with torch.no_grad():
+            speed_preds, crossing_preds, intentions = net(speed=obs_s, pos=obs_p, average=True)
+            speed_loss    = mse(speed_preds, target_s)/100
+
+            crossing_loss = 0
+            for i in range(target_c.shape[1]):
+                crossing_loss += bce(crossing_preds[:,i], target_c[:,i])
+            crossing_loss /= target_c.shape[1]
+
+            avg_epoch_val_s_loss += float(speed_loss)
+            avg_epoch_val_c_loss += float(crossing_loss)
+
+            preds_p = utils.speed2pos(speed_preds, obs_p)
+            ade += float(utils.ADE_c(preds_p, target_p))
+            fde += float(utils.FDE_c(preds_p, target_p))
+            aiou += float(utils.AIOU(preds_p, target_p))
+            fiou += float(utils.FIOU(preds_p, target_p))
+
+            target_c = target_c[:,:,1].view(-1).cpu().numpy()
+            crossing_preds = np.argmax(crossing_preds.view(-1,2).detach().cpu().numpy(), axis=1)
+
+            label_c = label_c.view(-1).cpu().numpy()
+            intentions = intentions.view(-1).detach().cpu().numpy()
+
+            state_preds.extend(crossing_preds)
+            state_targets.extend(target_c)
+            intent_preds.extend(intentions)
+            intent_targets.extend(label_c)
+
+        avg_epoch_val_s_loss += float(speed_loss)
+        avg_epoch_val_c_loss += float(crossing_loss)
+
+    avg_epoch_val_s_loss /= counter
+    avg_epoch_val_c_loss /= counter
+    val_s_scores.append(avg_epoch_val_s_loss)
+    val_c_scores.append(avg_epoch_val_c_loss)
+
+    ade  /= counter
+    fde  /= counter
+    aiou /= counter
+    fiou /= counter
+
+    avg_acc = accuracy_score(state_targets, state_preds)
+    avg_rec = recall_score(state_targets, state_preds, average='binary', zero_division=1)
+    avg_pre = precision_score(state_targets, state_preds, average='binary', zero_division=1)
+    mAP = average_precision_score(state_targets, state_preds, average=None)
+    intent_acc = accuracy_score(intent_targets, intent_preds)
+    intent_mAP = average_precision_score(intent_targets, intent_preds, average=None)
+
+    scheduler.step(crossing_loss)
+
+    print('e:', epoch+1, '| ts: %.4f'% avg_epoch_train_s_loss, '| tc: %.4f'% avg_epoch_train_c_loss,
+          '| vs: %.4f'% avg_epoch_val_s_loss, '| vc: %.4f'% avg_epoch_val_c_loss, '| ade: %.4f'% ade,
+          '| fde: %.4f'% fde, '| aiou: %.4f'% aiou, '| fiou: %.4f'% fiou, '| acc: %.4f'% avg_acc,
+          '| rec: %.4f'% avg_rec, '| pre: %.4f'% avg_pre, '| int_acc: %.4f'% intent_acc,
+          '| t:%.4f'%(time.time()-start))
+    print('*'*80)
+
+print('='*100)
+plt.figure(figsize=(10,8))
+plt.plot(list(range(len(train_s_scores))), train_s_scores, label = 'BB Training loss')
+plt.plot(list(range(len(val_s_scores))), val_s_scores, label = 'BB Validation loss')
+plt.plot(list(range(len(train_c_scores))), train_c_scores, label = 'Intention Training loss')
+plt.plot(list(range(len(val_c_scores))), val_c_scores, label = 'Intention Validation loss')
+plt.xlabel('epoch')
+plt.ylabel('Mean square error loss')
+plt.legend()
+plt.show()
+print('='*100)
+
+print('Testing ...')
+avg_epoch_val_s_loss   = 0
+avg_epoch_val_c_loss   = 0
+
+counter=0
+state_preds = []
+state_targets = []
+intent_preds = []
+intent_targets = []
+for idx, (obs_s, target_s, obs_p, target_p, target_c, label_c) in enumerate(test):
+    counter+=1
+    obs_s    = obs_s.to(device='cuda')
+    target_s = target_s.to(device='cuda')
+    obs_p    = obs_p.to(device='cuda')
+    target_p = target_p.to(device='cuda')
+    target_c = target_c.to(device='cuda')
+
+    with torch.no_grad():
+        speed_preds, crossing_preds, intentions = net(speed=obs_s, pos=obs_p, average=True)
+        speed_loss    = mse(speed_preds, target_s)/100
+
+        crossing_loss = 0
+        for i in range(target_c.shape[1]):
+            crossing_loss += bce(crossing_preds[:,i], target_c[:,i])
+        crossing_loss /= target_c.shape[1]
+
+        avg_epoch_val_s_loss += float(speed_loss)
+        avg_epoch_val_c_loss += float(crossing_loss)
+
+        preds_p = utils.speed2pos(speed_preds, obs_p)
+        ade += float(utils.ADE_c(preds_p, target_p))
+        fde += float(utils.FDE_c(preds_p, target_p))
+        aiou += float(utils.AIOU(preds_p, target_p))
+        fiou += float(utils.FIOU(preds_p, target_p))
+
+        target_c = target_c[:,:,1].view(-1).cpu().numpy()
+        crossing_preds = np.argmax(crossing_preds.view(-1,2).detach().cpu().numpy(), axis=1)
+
+        label_c = label_c.view(-1).cpu().numpy()
+        intentions = intentions.view(-1).detach().cpu().numpy()
+
+        state_preds.extend(crossing_preds)
+        state_targets.extend(target_c)
+        intent_preds.extend(intentions)
+        intent_targets.extend(label_c)
+
+    avg_epoch_val_s_loss += float(speed_loss)
+    avg_epoch_val_c_loss += float(crossing_loss)
+
+avg_epoch_val_s_loss /= counter
+avg_epoch_val_c_loss /= counter
+val_s_scores.append(avg_epoch_val_s_loss)
+val_c_scores.append(avg_epoch_val_c_loss)
+
+ade  /= counter
+fde  /= counter
+aiou /= counter
+fiou /= counter
+
+avg_acc = accuracy_score(state_targets, state_preds)
+avg_rec = recall_score(state_targets, state_preds, average='binary', zero_division=1)
+avg_pre = precision_score(state_targets, state_preds, average='binary', zero_division=1)
+intent_acc = accuracy_score(intent_targets, intent_preds)
+
+print('| vs: %.4f'% avg_epoch_val_s_loss, '| vc: %.4f'% avg_epoch_val_c_loss, '| ade: %.4f'% ade,
+      '| fde: %.4f'% fde, '| aiou: %.4f'% aiou, '| fiou: %.4f'% fiou, '| acc: %.4f'% avg_acc,
+      '| rec: %.4f'% avg_rec, '| pre: %.4f'% avg_pre, '| int_acc: %.4f'% intent_acc,
+      '| t:%.4f'%(time.time()-start))
--- utils.py
+++ utils.py
@@ -1,66 +1,29 @@
 import numpy as np
 import torch
-import torch.nn as nn
-from sklearn.metrics import recall_score, accuracy_score
-
-def ADE(pred, true):
-    pred_cx = pred[:,:,0]+pred[:,:,2]/2
-    pred_cy = pred[:,:,1]+pred[:,:,3]/2
-    true_cx = true[:,:,0]+true[:,:,2]/2
-    true_cy = true[:,:,1]+true[:,:,3]/2
-
-    displacement = torch.sqrt((pred_cx-true_cx)**2 + (pred_cy-true_cy)**2)
-    ade = torch.mean(displacement)
-
-    return ade
 
 
 def ADE_c(pred, true):
     displacement = torch.sqrt((pred[:,:,0]-true[:,:,0])**2 + (pred[:,:,1]-true[:,:,1])**2)
     ade = torch.mean(displacement)
-
+    
     return ade
+
+
+def FDE_c(pred, true):
+    displacement = torch.sqrt((pred[:,-1,0]-true[:,-1,0])**2 + (pred[:,-1,1]-true[:,-1,1])**2)
+    fde = torch.mean(displacement)
+    
+    return fde
 
 
 def AAE(pred, true):
     pred_a = pred[:,:,2] * pred[:,:,3]
     true_a = true[:,:,2] * true[:,:,3]
-
+    
     area_error = torch.abs(pred_a - true_a)
     aae = torch.mean(area_error)
-
+    
     return aae
-
-
-def FDE(pred, true):
-    pred_lx = pred[:,-1,0]+pred[:,-1,2]/2
-    pred_ly = pred[:,-1,1]+pred[:,-1,3]/2
-    true_lx = true[:,-1,0]+true[:,-1,2]/2
-    true_ly = true[:,-1,1]+true[:,-1,3]/2
-
-    displacement = torch.sqrt((pred_lx-true_lx)**2 + (pred_ly-true_ly)**2)
-    fde = torch.mean(displacement)
-
-    return fde
-
-
-def FIOU(pred, true):
-    min_pred = pred[:,-1,:2]-pred[:,-1,2:]/2
-    max_pred = pred[:,-1,:2]+pred[:,-1,2:]/2
-    min_true = true[:,-1,:2]-true[:,-1,2:]/2
-    max_true = true[:,-1,:2]+true[:,-1,2:]/2
-
-    min_inter = torch.max(min_pred, min_true)
-    max_inter = torch.min(max_pred, max_true)
-
-    interArea = torch.max(torch.zeros(min_inter.shape[0]).to('cuda'), (max_inter[:,0]-min_inter[:,0])) * \
-                torch.max(torch.zeros(max_inter.shape[0]).to('cuda'), (max_inter[:,1]-min_inter[:,1]))
-
-    pred_a = pred[:,-1,2] * pred[:,-1,3]
-    true_a = true[:,-1,2] * true[:,-1,3]
-
-    iou = torch.mean(interArea / (pred_a + true_a - interArea))
-    return float(iou)
 
 
 def AIOU(pred, true):
@@ -68,41 +31,58 @@ def AIOU(pred, true):
     max_pred = pred[:,:,:2]+pred[:,:,2:]/2
     min_true = true[:,:,:2]-true[:,:,2:]/2
     max_true = true[:,:,:2]+true[:,:,2:]/2
-
+    
     min_inter = torch.max(min_pred, min_true)
     max_inter = torch.min(max_pred, max_true)
 
-    interArea = torch.max(torch.zeros(min_inter.shape[0],min_inter.shape[1]).to('cuda'), (max_inter[:,:,0]-min_inter[:,:,0])) * \
+    interArea = torch.max(torch.zeros(min_inter.shape[0],min_inter.shape[1]).to('cuda'), (max_inter[:,:,0]-min_inter[:,:,0])) *\
                 torch.max(torch.zeros(max_inter.shape[0],max_inter.shape[1]).to('cuda'), (max_inter[:,:,1]-min_inter[:,:,1]))
 
     pred_a = pred[:,:,2] * pred[:,:,3]
     true_a = true[:,:,2] * true[:,:,3]
-
+    
     iou = torch.mean(interArea / (pred_a + true_a - interArea))
     return float(iou)
 
 
-def FDE_c(pred, true):
-    displacement = torch.sqrt((pred[:,-1,0]-true[:,-1,0])**2 + (pred[:,-1,1]-true[:,-1,1])**2)
-    fde = torch.mean(displacement)
+def FIOU(pred, true):
+    min_pred = pred[:,-1,:2]-pred[:,-1,2:]/2
+    max_pred = pred[:,-1,:2]+pred[:,-1,2:]/2
+    min_true = true[:,-1,:2]-true[:,-1,2:]/2
+    max_true = true[:,-1,:2]+true[:,-1,2:]/2
+    
+    min_inter = torch.max(min_pred, min_true)
+    max_inter = torch.min(max_pred, max_true)
+    
+    interArea = torch.max(torch.zeros(min_inter.shape[0]).to('cuda'), (max_inter[:,0]-min_inter[:,0])) * \
+                torch.max(torch.zeros(max_inter.shape[0]).to('cuda'), (max_inter[:,1]-min_inter[:,1]))
 
-    return fde
+    pred_a = pred[:,-1,2] * pred[:,-1,3]
+    true_a = true[:,-1,2] * true[:,-1,3]
+    
+    iou = torch.mean(interArea / (pred_a + true_a - interArea))
+    return float(iou)
 
 
 def compute_center(row):
     row['x'] = row['x'] + row['w']/2
     row['y'] = row['y'] + row['h']/2
-
+    
     return row
 
 
-def speed2pos(preds, obs_p, batch, device):
-    pred_pos = torch.zeros(preds.shape[0], args.seq_len, 4).to(args.device)
+def speed2pos(preds, obs_p):
+    pred_pos = torch.zeros(preds.shape[0], preds.shape[1], 4).to('cuda')
     current = obs_p[:,-1,:]
-    for i in range(args.seq_len):
+    for i in range(preds.shape[1]):
         pred_pos[:,i,:] = current + preds[:,i,:]
         current = pred_pos[:,i,:]
-
+        
+    pred_pos[:,:,0] = torch.min(pred_pos[:,:,0], 1920*torch.ones(pred_pos.shape[0], pred_pos.shape[1], device='cuda'))
+    pred_pos[:,:,1] = torch.min(pred_pos[:,:,1], 1080*torch.ones(pred_pos.shape[0], pred_pos.shape[1], device='cuda'))
+    pred_pos[:,:,0] = torch.max(pred_pos[:,:,0], torch.zeros(pred_pos.shape[0], pred_pos.shape[1], device='cuda'))
+    pred_pos[:,:,1] = torch.max(pred_pos[:,:,1], torch.zeros(pred_pos.shape[0], pred_pos.shape[1], device='cuda'))
+        
     return pred_pos
 
 
@@ -111,7 +91,7 @@ def compute_corners(bb):
     y_low_left = int(bb[1] - bb[3]/2)
     x_high_right = int(bb[0] + bb[2]/2)
     y_high_right = int(bb[1] + bb[3]/2)
-
+    
     return (x_low_left, y_low_left), (y_high_right, y_high_right)
 
 
@@ -119,11 +99,3 @@ def drawrect(drawcontext, bb, width=5):
     (x1, y1), (x2, y2) = bb
     points = (x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)
     drawcontext.line(points, fill="red", width=width)
-
-
-def recall(preds, target):
-    return recall_score(nn.Threshold(0.,0.)(nn.Threshold(-0.5, 1.)(-1*crossing_preds[:,:,0])).reshape(-1).detach().cpu().numpy(), target_c[:,:-1,0].reshape(-1).cpu().numpy(),
-                            average='binary', zero_division=1)
-
-def accuracy(preds, target):
-    return accuracy_score(nn.Threshold(0.,0.)(nn.Threshold(-0.5, 1.)(-1*crossing_preds[:,:,0])).reshape(-1).detach().cpu().numpy(), target_c[:,:-1,0].reshape(-1).cpu().numpy())
